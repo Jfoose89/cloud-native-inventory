c5c6ef1293a568fe85989d2d7ee22306f2492d2e
Jfoose89 <jordan.foose@chasacademy.se>
2026-05-09 16:35:49 +0200

    Wire up Key Vault with secret

--- CloudNativeInventory.Api/Program.cs
+++ CloudNativeInventory.Api/Program.cs
@@ -1,3 +1,4 @@
+using Azure.Identity;
 using CloudNativeInventory.Api.Data;
 using CloudNativeInventory.Api.Models;
 using Microsoft.EntityFrameworkCore;
@@ -8,13 +9,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi(); // .NET 9 OpenAPI
 
-// TODO (Del 4 i "Tips och förslag"): Konfigurera Azure Key Vault
-// Använd Managed Identity för att hämta hemligheter i produktion.
-// if (builder.Environment.IsProduction())
-// {
-//     var keyVaultUrl = new Uri(builder.Configuration["KeyVaultUrl"]!);
-//     builder.Configuration.AddAzureKeyVault(keyVaultUrl, new DefaultAzureCredential());
-// }
+var keyVaultUrl = builder.Configuration["KeyVault:Url"];
+
+if (!string.IsNullOrEmpty(keyVaultUrl))
+{
+    builder.Configuration.AddAzureKeyVault(
+        new Uri(keyVaultUrl),
+        new DefaultAzureCredential()
+    );
+}
 
 // Vi använder InMemory-databas lokalt
 builder.Services.AddDbContext<InventoryDbContext>(options =>
